26f0deb4bee206edb5bf5e673da7043106eb5c86
Danan Wijaya <dananwijaya1996@gmail.com>
2020-10-28 14:22:42 +0700

    first commit

---
+++ scraper.rb
@@ -0,0 +1,19 @@
+require 'httparty'
+require 'nokogiri'
+require 'byebug'
+
+def scraper
+  url = "https://news.detik.com/"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+
+  # newsFeed = parsed_page.css('div.section>div.list-content>article')
+  # first = newsFeed
+  # raw_title = first.css('div.media__text>h3.media__title>a').text
+  # title = raw_title.gsub(/\n/,'').gsub(/\t/, '').strip
+
+
+  byebug
+end
+
+scraper
